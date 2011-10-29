d2477e913b6879944c710db075abe47b7e059b23
Sebastian Ortega <sortega@tid.es>
2011-10-29 18:56:18 +0200

    Green

--- src/gol/Cell.java
+++ src/gol/Cell.java
@@ -1,21 +1,38 @@
 package gol;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Cell {
     private final boolean alive;
+    private final List<Cell> neightbours;
 
     public Cell(boolean alive) {
         this.alive = alive;
+        this.neightbours = new LinkedList<Cell>();
     }
 
     public boolean isAlive() {
         return alive;
     }
 
-    public void addNeightbour(Cell aliveNeightbour) {
+    public void addNeightbour(Cell neightbour) {
+        this.neightbours.add(neightbour);
     }
 
     public Cell next() {
-        return null;
+        return new Cell(this.aliveNeightbours() <= 3);
+    }
+
+    private int aliveNeightbours() {
+        int numAlive = 0;
+
+        for(Cell c : neightbours) {
+            if (c.isAlive()) {
+                numAlive++;
+            }
+        }
+        return numAlive;
     }
 
 }
