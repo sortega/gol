e9a1e868d591b5ed33b083c9efc0361a6708f4b1
Sebastian Ortega <sortega@tid.es>
2011-10-29 20:01:26 +0200

    Refactor

--- test/gol/WorldTest.java
+++ test/gol/WorldTest.java
@@ -5,27 +5,26 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class WorldTest {
+    private char[][] emptyWorld;
+    private char[][] fullWorld;
 
     @Before
     public void setUp() {
+        emptyWorld = new char[][] {{'.'}};
+        fullWorld = new char[][] {{'*'}};
     }
 
     @Test
     public void build1by1Worlds() {
-        char[][] emptyWorld = new char[][] {{'.'}};
         World instance = new World(emptyWorld);
         assertArrayEquals(emptyWorld, instance.getMatrix());
 
-        char[][] fullWorld = new char[][] {{'*'}};
         instance = new World(fullWorld);
         assertArrayEquals(fullWorld, instance.getMatrix());
     }
 
     @Test
     public void next1by1World() {
-        char[][] emptyWorld = new char[][] {{'.'}};
-        char[][] fullWorld = new char[][] {{'*'}};
-
         World instance = new World(fullWorld);
         assertArrayEquals(emptyWorld, instance.next().getMatrix());
     }
