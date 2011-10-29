99509ed2fdf45280a635ac14117a3a22b3553f5e
Sebastian Ortega <sortega@tid.es>
2011-10-29 19:18:02 +0200

    Red

--- test/gol/CellTest.java
+++ test/gol/CellTest.java
@@ -16,36 +16,56 @@ public class CellTest {
 
     @Before
     public void setUp() {
-        instance = new Cell(true);
     }
 
     @Test
     public void aliveAndDeadCells() {
+        instance = new Cell(true);
         assertTrue(new Cell(true).isAlive());
         assertFalse(new Cell(false).isAlive());
     }
 
     @Test
     public void deathByOverpopulation() {
+        instance = new Cell(true);
         addAliveNeightbours(4);
         assertFalse(instance.next().isAlive());
     }
 
     @Test
     public void deathByUnderpopulation() {
+        instance = new Cell(true);
         addAliveNeightbours(1);
         assertFalse(instance.next().isAlive());
     }
 
     @Test
     public void keptAliveWith2Neighbours() {
+        instance = new Cell(true);
         addAliveNeightbours(2);
         assertTrue(instance.next().isAlive());
     }
 
     @Test
     public void keptAliveWith3Neighbours() {
+        instance = new Cell(true);
         addAliveNeightbours(3);
         assertTrue(instance.next().isAlive());
+    }
+
+    @Test
+    public void respawnOnThreeAliveNeightbours() {
+        instance = new Cell(false);
+        addAliveNeightbours(3);
+        assertTrue(instance.next().isAlive());
+    }
+
+    @Test
+    public void keptDeathWithOtherThan3() {
+        instance = new Cell(false);
+        addAliveNeightbours(2);
+        assertFalse(instance.next().isAlive());
+        addAliveNeightbours(2); // 4!
+        assertFalse(instance.next().isAlive());
     }
 }
