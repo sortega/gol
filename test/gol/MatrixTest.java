65d9906ce89ae4e067817fe00b0f6ece1f94ef6b
Sebastian Ortega <sortega@tid.es>
2011-10-30 13:37:31 +0100

    Red

---
+++ test/gol/MatrixTest.java
@@ -0,0 +1,21 @@
+package gol;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MatrixTest {
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void buildEmptyMatrix() {
+        Matrix<Integer> instance = new Matrix(2, 2);
+        for (int i=0; i<2; i++)
+            for (int j=0; j<2; j++)
+                assertNull(instance.get(i, j));
+    }
+}
