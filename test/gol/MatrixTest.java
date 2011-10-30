d3167c8da04d391fae0e85afe339160e6a93be0d
Sebastian Ortega <sortega@tid.es>
2011-10-30 13:44:42 +0100

    Refactor

--- test/gol/MatrixTest.java
+++ test/gol/MatrixTest.java
@@ -5,13 +5,16 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MatrixTest {
+
+    private Matrix<Integer> instance;
+
     @Before
     public void setUp() {
     }
 
     @Test
     public void buildEmptyMatrix() {
-        Matrix<Integer> instance = new Matrix(2, 2);
+        instance = new Matrix(2, 2);
         for (int i=0; i<2; i++)
             for (int j=0; j<2; j++)
                 assertNull(instance.get(i, j));
@@ -19,7 +22,7 @@ public class MatrixTest {
 
     @Test
     public void modifyMatrix() {
-        Matrix<Integer> instance = new Matrix(1, 1);
+        instance = new Matrix(1, 1);
         instance.put(0, 0, 13);
         assertEquals((Integer) 13, instance.get(0, 0));
     }
